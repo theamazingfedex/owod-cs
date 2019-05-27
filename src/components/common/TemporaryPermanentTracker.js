dfee2aedc3fec6c6a30fbc53635d0cf894e6c90b
Daniel Wood <daniel.wood4@extendhealth.com>
2019-05-27 03:16:50 -0600

    add components and update visual styles * add temp/permanent tracker for faith/torment/willpower * add placeholder components for Merits and Flaws, as well as Health to aid in styling * update visual stying of the ability dots

--- src/components/common/TemporaryPermanentTracker.js
+++ src/components/common/TemporaryPermanentTracker.js
@@ -11,30 +11,60 @@ class TemporaryPermanentTracker extends Component {
     super(props);
 
     this.onTempChanged = this.onTempChanged.bind(this);
-    this.state = {
-      permanentValue: props.permanentValue,
-      temporaryValue: props.temporaryValue
-    }
+    this.onPermChanged = this.onPermChanged.bind(this);
   }
 
   onTempChanged(value) {
-    if (this.props.isTorment && value === this.state.permanentValue) {
-      // TODO: find out if temporary torment is lowered when permanent is increased.
-      this.setState({permanentValue: this.state.permanentValue + 1})
+    if (value === 1 && this.props.temporaryValue === 1) {
+      this.props.onChangedCallback({temporary: 0, permanent: this.props.permanentValue});
     }
-    if (!this.props.isTorment && value === 0) {
-      this.setState({permanentValue: this.state.permanentValue - 1});
-      // TODO: find out if temporary faith/willpower is reset when permanent is decreased.
+    else if (value <= this.props.permanentValue) {
+      this.props.onChangedCallback({temporary: value, permanent: this.props.permanentValue});
+    } else {
+      this.props.onChangedCallback({temporary: this.props.permanentValue, permanent: this.props.permanentValue});
+    }
+  }
+
+  onPermChanged(value) {
+    if (value === 1 && this.props.permanentValue === 1) {
+      this.props.onChangedCallback({temporary: 0, permanent: 0});
+    }
+    else if (value < this.props.temporaryValue) {
+      this.props.onChangedCallback({temporary: value, permanent: value});
+    } else {
+      this.props.onChangedCallback({temporary: this.props.temporaryValue, permanent: value});
     }
   }
 
   render() {
     return (
-      <div>
-        <h4>{this.props.label}</h4>
-        <Rating initialRating={this.state.value} emptySymbol={emptyCircularStyle} fullSymbol={fullCircularStyle} placeholderSymbol={fullCircularStyle} />
-        <Rating initialRating={this.state.value} emptySymbol={emptySquareStyle} fullSymbol={fullSquareStyle} placeholderSymbol={fullSquareStyle} onClick={this.onTempChange} />
+      <div className='temp-permanent-tracker'>
+        <div className='header'>
+          <div className='spacing'></div>
+          <h5>{this.props.label}</h5>
+          <div className='spacing'></div>
+        </div>
+        <Rating stop={10} initialRating={this.props.permanentValue} emptySymbol={emptyCircularStyle} fullSymbol={fullCircularStyle} placeholderSymbol={fullCircularStyle} onClick={this.onPermChanged}/>
+        <Rating stop={10} initialRating={this.props.temporaryValue} emptySymbol={emptySquareStyle} fullSymbol={fullSquareStyle} placeholderSymbol={fullSquareStyle} onClick={this.onTempChanged} />
         <style jsx>{`
+          .temp-permanent-tracker {
+            display: flex;
+            flex-direction: column;
+            min-width: 200px;
+          }
+          .header {
+            display: flex;
+          }
+          .spacing {
+            border-top: 2px solid black;
+            margin-top: 20px;
+            flex-grow: 2;
+          }
+          h5 {
+            margin: 10px;
+            align-self: center;
+            flex-grow: 0;
+          }
         `}</style>
       </div>
     );
