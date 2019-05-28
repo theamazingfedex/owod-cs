ce453db8d81996d65724dcf06590bd2e7deb2c7a
Daniel Wood <daniel.wood4@extendhealth.com>
2019-05-27 23:06:35 -0600

    Fix jest and enzyme autocomplete

--- setupEnzyme.js
+++ setupEnzyme.js
@@ -1,5 +1,6 @@
 import 'jest-enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { configure } from 'enzyme';
+import Enzyme, { configure } from 'enzyme';
 
 configure({ adapter: new Adapter() });
+export default Enzyme;
