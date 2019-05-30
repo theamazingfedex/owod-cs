d34de20f70990a25ad95220762edb96d4b124ba3
Joshua Homer <johomer@extendhealth.com>
2019-05-30 15:49:39 -0600

    added a few supporting packages, cleanup of project structure, modifed templates to use TypeScript

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,45 @@
 <template>
-  <v-app>
+  <v-app dark>
+    <v-navigation-drawer app fixed v-model="primaryDrawer"></v-navigation-drawer>
     <v-toolbar app>
+      <v-toolbar-side-icon @click="primaryDrawer = !primaryDrawer"></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>OWOD</span>
+        <span class="font-weight-light">Character designer</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
+      <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-toolbar>
-
     <v-content>
-      <HelloWorld/>
+      <router-view/>
     </v-content>
+    <v-footer app fixed inset class="pa-3">
+      owod-cs vueJS Implementation
+      <v-spacer/>
+      &copy; {{copyYear}} - Some copyright information
+    </v-footer>
   </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld'
+<style lang="scss">
+html {
+  overflow: hidden;
+}
+</style>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
-    }
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class App extends Vue {
+  public name: string = 'App';
+  public copyYear: number = new Date().getFullYear();
+  public primaryDrawer: boolean = false;
+  public mounted() {
+    this.primaryDrawer = false;
   }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <v-layout>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  </v-layout>
 </template>
 
 <script lang="ts">
